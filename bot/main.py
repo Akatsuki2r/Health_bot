567b5ece9ef1f3a07b75ef316626405ee9466eff
Mark Nahano <marknahano@gmail.com>
2025-05-07 11:34:42 +0300

    I dont remember what this is about

--- bot/main.py
+++ bot/main.py
@@ -4,6 +4,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from models.schemas import JournalEntry
+from logic.emotion import detect_emotions_and_symptoms
 
 bot = FastAPI()
 
@@ -24,7 +25,16 @@ async def read():
 @bot.post("/journal")
 async def write_journal(entry: JournalEntry):
     #Analyze journal text through the emotion.py
-    analysis = detect_emotions_and_symp
+    analysis = detect_emotions_and_symptoms(entry.entry)
+
+    #Returns the original entry + analysis
+    return {
+        "message": "Journal entry received",
+        "entry": entry.model_dump(),#gives us the dictionary form of the full JournalEntry (including timestamp)
+        "analysis": analysis
+    }
+
+
     
 
 
